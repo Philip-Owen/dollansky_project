b62768218c39e53c8273eab67a9f7e37449e217f
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-09-24 14:00:54 -0500

    finished contact form styling and layout

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -12,18 +12,13 @@ var Enquiry = new keystone.List('Enquiry', {
 });
 
 Enquiry.add({
-	name: { type: Types.Name, required: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
 	email: { type: Types.Email, required: true },
-	phone: { type: String },
-	enquiryType: { type: Types.Select, options: [
-		{ value: 'message', label: 'Just leaving a message' },
-		{ value: 'question', label: 'I\'ve got a question' },
-		{ value: 'other', label: 'Something else...' },
-	] },
 	message: { type: Types.Markdown, required: true },
 	createdAt: { type: Date, default: Date.now },
 });
 
 Enquiry.defaultSort = '-createdAt';
-Enquiry.defaultColumns = 'name, email, enquiryType, createdAt';
+Enquiry.defaultColumns = 'firstName, lastName, email, createdAt';
 Enquiry.register();
