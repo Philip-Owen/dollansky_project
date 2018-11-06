5f2bc6823bcb8f383620e519a5365a2b95efbfd6
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-11-06 10:07:05 -0600

    renamed home page to academy

--- routes/middleware.js
+++ routes/middleware.js
@@ -6,9 +6,10 @@ var _ = require('lodash');
 */
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
+		{ label: 'academy', key: 'academy', href: '/academy' },
 		{ label: 'about', key: 'about', href: '/about' },
 		{ label: 'equipment', key: 'equipment', href: '/equipment' },
-		{ label: 'register', key: 'register', href: '/register' },
+		// { label: 'register', key: 'register', href: '/register' },
 		{ label: 'contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.home = [
