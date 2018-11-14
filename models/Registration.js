8eeab2ce4837dbb6493e61b430d1ed07b2fc30b2
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-11-14 14:11:18 -0600

    reconfigured register page based off new designs

--- models/Registration.js
+++ models/Registration.js
@@ -14,8 +14,6 @@ var Registration = new keystone.List('Registration', {
 
 // COMBAK: This will need to be refactored for use payment gateway. This is a temp mockup.
 Registration.add({
-	programName: { type: String, required: false },
-	programPrice: { type: Number, required: false },
 	personalInfo: {
 		firstName: { type: String, required: true },
 		lastName: { type: String, required: true },
@@ -23,21 +21,9 @@ Registration.add({
 		phone: { type: String, required: true },
 		experience: { type: String, required: true },
 	},
-	paymentInfo: {
-		firstName: { type: String, required: true },
-		lastName: { type: String, required: true },
-		cardNumber: { type: Number, required: true },
-		expDate: { type: String, required: true },
-		cvv: { type: Number, required: true },
-		billingAddress: { type: String, required: true },
-		billingAddress2: { type: String, required: false },
-		city: { type: String, required: true },
-		state: { type: String, required: true },
-		zipcode: { type: Number, required: true },
-	},
 	submitted: { type: Date, default: Date.now },
 });
 
 Registration.defaultSort = '-submitted';
-Registration.defaultColumns = 'programName, programPrice, personalInfo, paymentInfo, submitted';
+Registration.defaultColumns = 'personalInfo.firstName, personalInfo.lastName, personalInfo.email, personalInfo.phone, personalInfo.experience, submitted';
 Registration.register();
